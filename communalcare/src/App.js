dcadbeaf01112ac52b06be5beaa5d7457a60e7db
Yangtsel Sherap <yangtsel.sherap@gmail.com>
2022-09-06 10:25:06 -0400

    added AUthenticated and User usestates in app.js

--- communalcare/src/App.js
+++ communalcare/src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
+import {useState} from 'react'
 import Home from './pages/Home/Home'
 import Navbar from './components/Navbar/Navbar'
 import Login from './pages/Login/Login'
@@ -10,10 +11,22 @@ import OffersPage from './pages/OffersPage/OffersPage'
 import RequestPage from './pages/RequestsPage/RequestsPage'
 
 const App = () => {
+  const [authenticated, toggleAuthenticated] = useState(false)
+  const [user, setUser] = useState(null)
+
+  const handleLogout = () => {
+    setUser(null)
+    toggleAuthenticated(false)
+    localStorage.clear()
+  }
 
   return (
     <div>
-        <Navbar />
+        <Navbar 
+          authenticated={authenticated}
+          user={user}
+          handleLogout={handleLogout}
+        />
       <main>
         <Routes>
           <Route path="/" element={ <Home />} />
