ac76f6978995cde0ebf3e71e9f8406c7218211a1
Yangtsel Sherap <yangtsel.sherap@gmail.com>
2022-09-06 09:55:55 -0400

    basic registration form

--- communalcare/src/components/OfferCard/OfferCard.jsx
+++ communalcare/src/components/OfferCard/OfferCard.jsx
@@ -1,19 +1,7 @@
-// const OfferCard = () => {
-
-//   return (
-//     <div>
-//       <h1>offers</h1>
-//     </div>
-//   )
-// }
-
-// export default OfferCard
-
-
 const OfferCard = ({offer, index}) => {
 
   return (
-    <div>
+    <div id="offerCard">
       <h2>title: {offer.title}</h2>
       <h3>condition: {offer.condition}</h3>
       <h3>photo: {offer.photo}</h3>
--- communalcare/src/pages/Register/Register.jsx
+++ communalcare/src/pages/Register/Register.jsx
@@ -1,8 +1,69 @@
+import React, {useState} from 'react'
+import './register.css'
+
 const Register = () => {
 
+  const initialState = {
+    firstName: '',
+    lastName: '',
+    username: '',
+    email: '',
+    password: ''
+  }
+
+  const [formState, setFormState] = useState(initialState)
+  
+  const handleChange = e => {
+    setFormState({ ...formState, [e.target.id]: e.target.value})
+  }
+  
+  const handleSubmit = e => {
+    e.preventDefault()
+    setFormState(initialState)
+  }
+
   return (
-    <div>
-      <h1>register</h1>
+    <div id="registration">
+      <h1>Become a member</h1>
+      <form onSubmit={handleSubmit} id="registrationForm">
+      <label htmlFor="firstName">First Name:</label> 
+        <input
+          id="firstName"
+          type="text"
+          onChange={handleChange}
+          value={formState.firstName}
+        />
+      <label htmlFor="lastName">Last Name:</label>
+        <input
+          id="lastName"
+          type="text"
+          onChange={handleChange}
+          value={formState.lastName}
+        />
+      <label htmlFor="username">Username:</label>
+        <input
+          id="username"
+          type="text"
+          onChange={handleChange}
+          value={formState.username}
+        />
+      <label htmlFor="email">Email:</label>
+        <input
+          id="email"
+          type="text"
+          onChange={handleChange}
+          value={formState.email}
+        />
+      <label htmlFor="password">Password:</label>
+        <input
+          id="password"
+          type="text"
+          onChange={handleChange}
+          value={formState.password}
+        />
+
+      <button type="submit">Submit</button>
+      </form>
     </div>
   )
 
