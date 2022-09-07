93009729c401a9d4dc2a4bb67bb9f226ea07565a
Yangtsel Sherap <yangtsel.sherap@gmail.com>
2022-09-07 10:57:52 -0400

    added Create functionality for Offers

--- communalcare/src/components/OfferCard/OfferCard.jsx
+++ communalcare/src/components/OfferCard/OfferCard.jsx
@@ -1,5 +1,5 @@
-import axios from 'axios'
-import { useState, useEffect } from 'react'
+// import axios from 'axios'
+// import { useState, useEffect } from 'react'
 import './offercard.css'
 
 const OfferCard = ({offer, key, id, removeOffer, index}) => {
@@ -7,6 +7,8 @@ const OfferCard = ({offer, key, id, removeOffer, index}) => {
   return (
     <div id="offerCard">
       <h2>title: {offer.title}</h2>
+      <h4>date: {offer.datePosted}</h4>
+      <img src={offer.photo} alt="mountain" />
       <button onClick={() => removeOffer(offer.id)} id="removeOffer">testing delete offer</button>
     </div>
   )
--- communalcare/src/pages/OffersPage/OffersPage.js
+++ communalcare/src/pages/OffersPage/OffersPage.js
@@ -5,6 +5,88 @@ import { useState} from 'react'
 const OfferPage = ( {offers, user, authenticated}) => {
 
   const [userOffers, setUserOffers] = useState([])
+  const [datePosted, setDatePosted] = useState('')
+  const [photo, setPhoto] = useState('')
+  const [title, setTitle] = useState('')
+  const [category, setCategory] = useState('')
+  const [condition, setCondition] = useState('')
+  const [borough, setBorough] = useState('')
+  const [zipcode, setZipcode] = useState('')
+  const [description, setDescription] = useState('')
+
+  const addOffer = async (e) => {
+    const res = await Client.post(`http://localhost:3001/offer/${user.id}`, {
+      datePosted: datePosted,
+      photo: photo,
+      title: title,
+      category: category,
+      condition: condition,
+      borough: borough,
+      zipcode: zipcode,
+      description: description
+    })
+    setDatePosted ('')
+    setPhoto ('')
+    setTitle ('')
+    setCategory ('')
+    setCondition ('')
+    setBorough ('')
+    setZipcode ('')
+    setDescription ('')
+    let tempArray = [...offers]
+    let tempObj = { ...res.data }
+    tempArray.push(tempObj)
+    setUserOffers(tempArray)
+  }
+
+   const handleSubmit = (e) => {
+    e.preventDefault()
+    addOffer(e)
+    window.location.reload(false)
+  }
+
+  const changeDatePosted = (event) => {
+    let e = event.target.value
+    setDatePosted(e)
+  }
+
+    const changePhoto = (event) => {
+    let e = event.target.value
+    setPhoto(e)
+  }
+
+  const changeTitle = (event) => {
+    let e = event.target.value
+    setTitle(e)
+  }
+
+  const changeCategory = (event) => {
+    let e = event.target.value
+    setCategory(e)
+  }
+
+  const changeCondition = (event) => {
+    let e = event.target.value
+    setCondition(e)
+  }
+
+  const changeBorough = (event) => {
+    let e = event.target.value
+    setBorough(e)
+  }
+
+  const changeZipcode = (event) => {
+    let e = event.target.value
+    setZipcode(e)
+  }
+
+  const changeDescription = (event) => {
+    let e = event.target.value
+    setDescription(e)
+  }
+
+
+
 
   const removeOffer = async (id, index) => {
     await Client.delete(`http://localhost:3001/offer/${id}`)
@@ -16,14 +98,107 @@ const OfferPage = ( {offers, user, authenticated}) => {
 
   return (user, authenticated) ? (
     <div>
-      {offers.map((offer, index) => (
-          <OfferCard 
-            offer={offer}
-            index={index}
-            removeOffer={removeOffer}
-          />  
-        )
-      )}   
+      <div className="formContainer">
+        <div id = "Form">
+        <h3 id="offerPageTitle">offers</h3>
+          <form onSubmit={handleSubmit}>
+            <div className="createOfferForm">
+              <div id = "offerInner">
+                <div>
+                  <input className='offerFromInput'
+                    type="date"
+                    value={datePosted}
+                    onChange={changeDatePosted}
+                    name={'name'}
+                    id="createOfferName"
+                  />
+                </div>
+                <div>
+                  <input className='offerFromInput'
+                    type="text"
+                    value={photo}
+                    onChange={changePhoto}
+                    name={'photo'}
+                    placeholder={'Photo'}
+                    id="createOfferPhoto"
+                  />
+                </div>
+                <div>
+                  <input className='offerFromInput'
+                    type="text"
+                    value={title}
+                    onChange={changeTitle}
+                    name={'title'}
+                    placeholder={'title'}
+                    id="createOfferTitle"
+                  />
+                </div>
+                <div>
+                  <input className='offerFromInput'
+                    type="text"
+                    value={category}
+                    onChange={changeCategory}
+                    name={'category'}
+                    placeholder={'category'}
+                    id="createOfferCategory"
+                  />
+                </div>
+                <div>
+                  <input className='offerFromInput'
+                    type="text"
+                    value={condition}
+                    onChange={changeCondition}
+                    name={'condition'}
+                    placeholder={'condition'}
+                    id="createOfferCondition"
+                  />
+                </div>
+                <div>
+                  <input className='offerFromInput'
+                    type="text"
+                    value={borough}
+                    onChange={changeBorough}
+                    name={'borough'}
+                    placeholder={'borough'}
+                    id="createOfferBorough"
+                  />
+                </div>
+                <div>
+                  <input className='offerFromInput'
+                    type="text"
+                    value={zipcode}
+                    onChange={changeZipcode}
+                    name={'zipcode'}
+                    placeholder={'zipcode'}
+                    id="createOfferZipcode"
+                  />
+                </div>
+                <div>
+                  <input className='offerFromInput'
+                  type="text"
+                    value={description}
+                    onChange={changeDescription}
+                    name={'description'}
+                    placeholder={'description'}
+                    id="createOfferDescription"
+                  />
+                </div>
+                <div>
+                <button id="form-submit" disabled={!title}>Submit</button>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+        {offers.map((offer, index) => (
+            <OfferCard 
+              offer={offer}
+              index={index}
+              removeOffer={removeOffer}
+            />  
+          )
+        )}   
     </div>
   ) : 
   <div>
