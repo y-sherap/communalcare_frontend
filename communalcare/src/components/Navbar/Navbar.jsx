ed7212ec2f9b9be58c76df15f985ddddfe7b091b
Yangtsel Sherap <yangtsel.sherap@gmail.com>
2022-09-08 12:52:21 -0400

    added conditional rendering of links in navbar

--- communalcare/src/components/Navbar/Navbar.jsx
+++ communalcare/src/components/Navbar/Navbar.jsx
@@ -2,20 +2,41 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './navbar.css'
 
-const Navbar = () => {
+const Navbar = ({user, authenticated}) => {
 
-  return (
-    <div id="navbar">
-      <nav id="navbarContainer">
-        <Link to="/" id="homeTitle">Home</Link>
-        <Link to="/dashboard" id="dashboardTitle">Dashboard</Link>
-        <Link to="/login" id="loginTitle">Login</Link>
-        <Link to="/register" id="registerTitle">Register</Link>
-        <Link to="/nyc-mutual-aid-resources" id="mutualaidTitle">Mutual Aid Resources</Link>
-        <Link to="/offers" id="offersTitle">Offers</Link>
-        <Link to="/requests" id="requestsTitle">Requests</Link>
-      </nav>
+  return (user && authenticated) ? (
+    <header>
+      <nav>
+        <div id="navbar">
+          <div>
+            <Link to="/" id="homeTitle">Home</Link>
+          </div>
+          <div>
+            <Link to="/dashboard" id="dashboardTitle">Dashboard</Link>
+            <Link to="/nyc-mutual-aid-resources" id="mutualaidTitle">Mutual Aid Resources</Link>
+            <Link to="/offers" id="offersTitle">Offers</Link>
+            <Link to="/requests" id="requestsTitle">Requests</Link>
+          </div>
+      </div>
+    </nav>
+    </header>
+  ) : (
+    <header>
+    <nav>
+      <div id="navbar">
+        <div>
+          <Link to="/" id="homeTitle">Home</Link>
+        </div>
+        <div>
+          <Link to="/login" id="loginTitle">Login</Link>
+          <Link to="/register" id="registerTitle">Register</Link>
+          <Link to="/nyc-mutual-aid-resources" id="mutualaidTitle">Mutual Aid Resources</Link>
+          <Link to="/offers" id="offersTitle">Offers</Link>
+          <Link to="/requests" id="requestsTitle">Requests</Link>
+        </div>
     </div>
+  </nav>
+  </header>
   )
 }
 
